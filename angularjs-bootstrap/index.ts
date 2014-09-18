f8ad928dc1951ea3cdd98a7526ed29209e72438f
Lucien Zuercher <lucien.zuercher@siemens.com>
2014-09-18 15:34:22 +0200

    example angularjs-bootstrap added

---
+++ angularjs-bootstrap/index.ts
@@ -0,0 +1,8 @@
+﻿///<reference path="typings/tsd.d.ts" />
+// ===================================================
+// IMPORTANT: only for development
+// total.js - web application framework for node.js
+// http://www.totaljs.com
+// ===================================================
+
+require('total.js').http('debug');
