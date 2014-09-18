649573ca68b2648c78aadea469753af0d848923b
Lucien Zuercher <lucien.zuercher@siemens.com>
2014-09-18 15:03:55 +0200

    setup view example

---
+++ views/index.ts
@@ -0,0 +1,9 @@
+﻿///<reference path="typings/tsd.d.ts" />
+// ===================================================
+// IMPORTANT: only for development
+// total.js - web application framework for node.js
+// http://www.totaljs.com
+// ===================================================
+
+import total = require("total.js");
+total.http("debug");
