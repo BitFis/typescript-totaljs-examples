92619fcce365c1e4bbf736dbf978cb4826ae9026
Lucien Zuercher <lucien.zuercher@siemens.com>
2014-09-25 10:18:12 +0200

    new apporach using class to route

--- angularjs-routing/app/app.ts
+++ angularjs-routing/app/app.ts
@@ -1,5 +1,29 @@
 ﻿var app = angular.module('app', ['ngRoute']);
 
+class Route {
+
+    constructor(public $routeProvider: ng.route.IRouteProvider, public $locationProvider: ng.ILocationProvider) {
+        $locationProvider.html5Mode(true);
+
+        $routeProvider
+            .when('/', {
+                templateUrl: '/views/home.html',
+                controller: 'HomeCtrl'
+            })
+            .when('/users/', {
+                templateUrl: '/views/users.html',
+                controller: 'UsersCtrl'
+            })
+            .when('/products/', {
+                templateUrl: '/views/products.html',
+                controller: 'ProductsCtrl'
+            })
+            .otherwise({ redirectTo: '/' });
+    }
+}
+
+app.config(Route);/*
+
 app.config(function ($routeProvider: ng.route.IRouteProvider, $locationProvider: ng.ILocationProvider) {
 
     $locationProvider.html5Mode(true);
@@ -15,4 +39,4 @@ app.config(function ($routeProvider: ng.route.IRouteProvider, $locationProvider:
         controller: 'ProductsCtrl'
     }).otherwise({ redirectTo: '/' });
 
-});
+});*/
