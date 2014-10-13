4e0050180a7e8fcb5f3897574fc08426a80078ef
Lucien Zuercher <lucien.zuercher@siemens.com>
2014-10-13 10:17:53 +0200

    adding routing example

---
+++ routing/controllers/default.ts
@@ -0,0 +1,94 @@
+﻿class Default extends TotalJS.Controller {
+
+    public static install() {
+        var self = Default.prototype;
+
+        // Documentation: http://docs.totaljs.com/Framework/#framework.route
+        framework.route('/', self.view_homepage);
+        framework.route('/contact/', self.view_contact);
+        framework.route('/products/', self.view_products);
+        framework.route('/products/{category}/', self.view_products);
+        framework.route('/products/{category}/{subcategory}/', self.view_products);
+        framework.route('/{category}/', self.view_homepage);
+
+        // this route has a lower priority and it will be executed when:
+        // url: /asterix/
+        // url: /asterix/bla/bla/bla/bla/
+        framework.route('/asterix/*', self.view_asterix);
+
+        // route: all txt files
+        // Documentation: http://docs.totaljs.com/Framework/#framework.file
+        // Try: http://127.0.0.4/test.txt
+        framework.file('All *.txt', self.static_txt);
+
+        // route: all jpg files
+        // all images will resized about 50%
+        // Documentation: http://docs.totaljs.com/Framework/#framework.file
+        // Try: http://127.0.0.4/header.jpg
+        framework.file('All *.jpg', self.static_jpg);
+    }
+
+    static_txt(req: any, res: any, isValidation: boolean) {
+        if (isValidation)
+            return req.url.indexOf('.txt') !== -1;
+
+        // generate response
+        // this === framework
+        // Documentation: http://docs.totaljs.com/Framework/#framework.responsContent
+        framework.responseContent(req, res, 200, 'Server time: ' + new Date().toString(), 'text/plain');
+    }
+
+    static_jpg(req: any, res: any, isValidation: boolean) {
+
+        if (isValidation)
+            return req.url.indexOf('.jpg') !== -1;
+
+        // generate response
+        // this === framework
+        // Documentation: http://docs.totaljs.com/Framework/#framework.responseImage
+        framework.responseImage(req, res, <string>this.path.public(req.url), function (image) {
+
+            // image === FrameworkImage
+            // http://docs.totaljs.com/FrameworkImage/
+
+            image.resize('50%');
+            image.quality(80);
+            image.minify();
+
+        });
+    }
+
+    view_asterix() {
+        var self = this;
+        self.plain(<string>'asterix -> ' + (<any>self.uri).pathname);
+    }
+
+    view_products(category: string, subcategory: string) {
+
+        category = category || '';
+        subcategory = subcategory || '';
+
+        if (category.length > 0)
+            category = ' -> ' + category;
+
+        if (subcategory.length > 0)
+            subcategory = ' -> ' + subcategory;
+
+        this.plain(<string>'products{0}{1}'.format(category, subcategory));
+    }
+
+    view_homepage(category: string) {
+
+        category = category || '';
+
+        if (category.length > 0)
+            category = ' -> ' + category;
+
+        this.plain(<string>'homepage{0}'.format(category));
+    }
+
+    view_contact() {
+        this.plain('contact');
+    }
+}
+export = Default;
---
+++ routing/readme.md
@@ -0,0 +1,68 @@
+# How to run it?
+
+## First step - Compile
+
+Install typescript globaly
+```
+npm install -g typescript 
+```
+
+Global installation of tsd to get up to date typescript definitions
+``` bash
+npm install -g tsd
+```
+
+Install dependencies
+``` bash
+npm install
+```
+
+Load definition files
+``` bash
+tsd update -os
+```
+
+### Using Visual Studio
+
+Open the solution _TotalJSTypescriptExamples.sln_ and compile this project
+
+### Using Grunt
+
+Install grunt-cli
+``` bash
+npm install grunt-cli -g
+```
+
+Run Grunt
+``` bash
+grunt
+```
+
+### Install GraphicsMagic
+
+This example uses the GraohicMagic library for one function (displaying jpg image). To get this
+working, you need to do following. 
+
+Install either [GraphicsMagick](http://www.graphicsmagick.org/) or [ImageMagick](http://www.imagemagick.org/).
+gm nodejs module is installed with the __npm install__ command.
+
+checkout the [gm-documentation / project](http://aheckmann.github.io/gm/) for information and usage.
+
+## Second step - Start server
+
+Run server
+``` bash
+node index
+```
+
+Open browser: <http://127.0.0.1:8000>
+
+
+### compile and start server
+
+use following oneliner to compile ts and run server (only possible with grunt)
+requires: Typescript & tsd & grunt installed
+``` bash
+npm install && tsd update -os && grunt && node test
+```
+
