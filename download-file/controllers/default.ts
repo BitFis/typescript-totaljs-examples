3f5e8758375e159729751811ef80d433409a4881
Lucien Zuercher <lucien.zuercher@siemens.com>
2014-10-21 11:02:27 +0200

    download-file example added

---
+++ download-file/controllers/default.ts
@@ -0,0 +1,26 @@
+﻿///<reference path="../typings/tsd.d.ts" />
+
+class Default extends TotalJS.Controller {
+
+    static install() {
+        var self = Default.prototype;
+        framework.route('/', self.file_download);
+        framework.route('/image/', self.image_download);
+    }
+
+    public file_download() {
+        // Documentation: http://docs.totaljs.com/FrameworkController/#controller.file
+	    this.file('totaljs.pdf', 'logo.pdf');
+    }
+
+    public image_download() {
+        // Documentation: http://docs.totaljs.com/FrameworkController/#controller.image
+        this.image('slovakia.jpg', function (image) {
+            image.resize('50%');
+            image.minify();
+        });
+    }
+
+}
+
+exports.install = Default.install;
---
+++ download-file/index.ts
@@ -0,0 +1,11 @@
+﻿///<reference path="./typings/tsd.d.ts" />
+
+// IMPORTANT: only for development
+// total.js - web application framework for node.js
+// http://www.totaljs.com
+// ===================================================
+
+// predefine classes, else errors will occure
+global.TotalJS = { Controller: {} };
+
+require('total.js').http('debug');
